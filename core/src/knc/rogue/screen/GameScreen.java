b4a166c3b5b171fb533df704c0f3f86c0ddf2249
knickus <robin_rtl@hotmail.com>
2020-03-11 23:06:30 +0100

    A crude console is now displayed

--- core/src/knc/rogue/screen/GameScreen.java
+++ core/src/knc/rogue/screen/GameScreen.java
@@ -38,6 +38,8 @@ public class GameScreen implements Screen {
                       new MapObjectRenderSystem(),
                       new ItemRenderSystem(),
                       new CharacterRenderSystem(),
+                      new UIRenderSystem(),
+
                       new ComponentCleanupSystem()
                 );
 
--- core/src/knc/rogue/system/view/render/UIRenderSystem.java
+++ core/src/knc/rogue/system/view/render/UIRenderSystem.java
@@ -1,8 +1,30 @@
 package knc.rogue.system.view.render;
 
-public class UIRenderSystem extends RenderSystem {
-    @Override
-    protected void process(int entityId) {
+import com.artemis.BaseSystem;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import squidpony.squidgrid.gui.gdx.SquidMessageBox;
 
+public class UIRenderSystem extends BaseSystem {
+    SquidMessageBox messageBox = new SquidMessageBox(20, 10);
+    SpriteBatch uiBatch = new SpriteBatch();
+
+    @Override
+    protected void begin() {
+        super.begin();
+
+        uiBatch.begin();
+    }
+
+    @Override
+    protected void processSystem() {
+        messageBox.appendMessage("Hello friend");
+        messageBox.draw(uiBatch, 1f);
+    }
+
+    @Override
+    protected void end() {
+        super.end();
+
+        uiBatch.end();
     }
 }
